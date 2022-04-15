138e80e704ca568975b707969534f760692a5253
shahar.ben <shahar.ben@campus.technion.ac.il>
2022-04-15 13:43:35 +0300

    created RLEListCreate func but it is not tested yet

--- RLEList.c
+++ RLEList.c
@@ -1,7 +1,23 @@
 #include "RLEList.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 struct RLEList_t{
-    //TODO: implement
+    int length;
+    char letter;
+    struct node* next;
 };
-hi
+
 //implement the functions here
+
+RLEList RLEListCreate() {
+
+        RLEList ptr = malloc(sizeof(*ptr));
+        if (!ptr) {
+            return NULL;
+        }
+        ptr->length = 0;
+        ptr->letter = '\0';
+        ptr->next = NULL;
+        return ptr;
+    }
