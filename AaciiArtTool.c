4b488ac02b51b884c7702765a42a59ffbc8068bf
talrotshtein <talrotshtein@campus.technion.ac.il>
2022-04-21 12:27:16 +0300

    creared asciiarttool.h and AsciiArtTool.c

---
+++ AaciiArtTool.c
@@ -0,0 +1,15 @@
+//
+// Created by Tal on 4/21/2022.
+//
+
+#include "AsciiArtTool.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+RLEList asciiArtRead(FILE* in_stream)
+{
+    if(in_stream == NULL)
+    {
+        return NULL;
+    }
+}
