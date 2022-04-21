eb32245d98074a17d5835b072b62960108e4d27e
shahar.ben <shahar.ben@campus.technion.ac.il>
2022-04-21 12:59:17 +0300

    created asciiArtPrint func

--- AaciiArtTool.c
+++ AaciiArtTool.c
@@ -13,3 +13,22 @@ RLEList asciiArtRead(FILE* in_stream)
         return NULL;
     }
 }
+
+
+RLEListResult asciiArtPrint(RLEList list, FILE *out_stream)
+{
+    RLEList real_list = list->next;
+    if(list==NULL || out_stream==NULL)
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    while(real_list)
+    {
+        for(int i=0;i<real_list->size;i++)
+        {
+            fprintf(out_stream, "%c",real_list->value);
+        }
+        real_list = real_list->next;
+    }
+    return RLE_LIST_SUCCESS;
+}
